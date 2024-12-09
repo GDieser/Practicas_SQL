563d6159da7508fe2e4d087f0ef27115575f0f3e
GDieser <germandieser@gmail.com>
2024-12-09 11:05:54 -0300

    Se agregaron los datos para la practica

---
+++ MundoPokemonDB/SQLQuery7.sql
@@ -0,0 +1,24 @@
+
+Select * From Pokemons
+
+Delete From Elementos
+
+Select * From Elementos
+
+Update Elementos set Descripcion = 'Agua' where Id = 3
+
+Insert into Elementos values (7, 'Fantasma')
+Insert into Elementos values (8, 'Tierra')
+Insert into Elementos values (9, 'Hielo')
+Insert into Elementos values (10, 'Veneno')
+Insert into Elementos values (11, 'Psiquico')
+Insert into Elementos values (12, 'Roca')
+Insert into Elementos values (13, 'Siniestro')
+Insert into Elementos values (14, 'Dragon')
+Insert into Elementos values (15, 'Lucha')
+Insert into Elementos values (16, 'Hada')
+Insert into Elementos values (17, 'Bicho')
+
+Select * From Habilidades
+
+Delete From Habilidades
